5dd305ffb29cd164aba635214a5e0a61ac848a95
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2020-05-29 15:24:41 +0700

    boilerplate code

---
+++ src/utils/assertSnapshots.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+/**
+ * @param Component - (Component) Component that needs to be test.
+ * @param testConfiguration - (object) object of props and test description
+ */
+const assertSnapshot = (Component, testConfiguration) => {
+  const { props, description } = testConfiguration;
+
+  it(description, () => {
+    const component = renderer.create(<Component {...props} />).toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+};
+
+const assertSnapshots = (Component, testConfigurations) => {
+  testConfigurations.forEach((testConfiguration) => assertSnapshot(Component, testConfiguration));
+};
+
+export default assertSnapshots;
