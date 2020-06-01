50a07aa08c95ac81dd91b408feb6563b55a6c6d2
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2020-06-01 10:59:16 +0700

    update failed snapshot testing on VacancyListComponent

--- src/Components/Vacancy/VacancyListComponent.snap.test.js
+++ src/Components/Vacancy/VacancyListComponent.snap.test.js
@@ -3,8 +3,12 @@ import VacancyListComponent from './VacancyListComponent';
 import VACANCIES from '../../fixtures/vacanciesData';
 
 jest.mock('../../utils/numberFormat', () => ({
-  currencyFormat: () => 'IDR 0.00'
-}));
+  currencyFormat: jest.fn().mockImplementation(() => 'IDR 0.00')
+}))
+  .mock('moment', () => () => ({
+    fromNow: jest.fn().mockImplementation(() => '1 days ago'),
+    format: jest.fn().mockImplementation(() => '1 Jan')
+  }));
 
 describe('VacancyListComponent snapshot', () => {
   const configurations = [
